d4e8e2f4578ce08cbda70415207332f0a997df5f
leonardobarral <leonardobarral2@gmail.com>
2024-07-31 00:01:20 -0300

    Bulding applications

---
+++ src/main/java/com/genesisforhealth/questionarios/QuestionariosApplication.java
@@ -0,0 +1,13 @@
+package com.genesisforhealth.questionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genesisforhealth/questionarios/QuestionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genesisforhealth.questionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
